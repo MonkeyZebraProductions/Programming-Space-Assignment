237feef9ea94a837f5422a829b0ffe63d351aa5d
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2022-02-20 17:36:57 +0000

    Banking

--- Programming Space Ship/Assets/Scripts/BigBoid.cs
+++ Programming Space Ship/Assets/Scripts/BigBoid.cs
@@ -7,7 +7,8 @@ public class BigBoid : MonoBehaviour
     public Vector3 acceleration;
     public Vector3 velocity;
     public Vector3 force;
-    public float speed;
+    public float speed, banking;
+    public float bankRatio=0.01f;
     public float mass = 1;
     public float maxSpeed = 10;
     public float approachModifier=2;
@@ -17,6 +18,19 @@ public class BigBoid : MonoBehaviour
 
     public bool seekEnabled = true; 
     public Transform seekTarget;
+
+    //If the boid is Following
+    public bool IsFollowPath;
+    public float FollowSpeed = 10;
+    public Transform PathTarget;
+
+    //If the boid is Persuing
+    public bool IsPersuing;
+    public float PersueSpeed = 20;
+    public Transform PersueTarget;
+
+    public Transform ModelCollision;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -54,7 +68,8 @@ public class BigBoid : MonoBehaviour
 
     void FollowPath()
     {
-        
+        seekTarget = PathTarget;
+        maxSpeed = FollowSpeed;
         if (Vector3.Distance(transform.position, seekTarget.position)<=path.distance)
         {
             index++;
@@ -69,11 +84,35 @@ public class BigBoid : MonoBehaviour
         }
     }
 
+    void Persue()
+    {
+        seekTarget = PersueTarget;
+        maxSpeed = PersueSpeed;
+    }
+
+    //Calculates rotation around Z axis
+    float BankValue()
+    {
+        Vector3 targetDir = seekTarget.position-transform.position;
+        banking = Mathf.Abs(Vector3.SignedAngle(targetDir, transform.position, Vector3.forward)*bankRatio);
+        return banking;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        FollowPath();
-        Approach();
+        if(IsFollowPath)
+        {
+            IsPersuing = false;
+            FollowPath();
+            Approach();
+        }
+        
+        if(IsPersuing)
+        {
+            IsFollowPath = false;
+            Persue();
+        }
         force = Calculate();
         acceleration = force / mass;
         velocity += acceleration * Time.deltaTime;
@@ -81,9 +120,13 @@ public class BigBoid : MonoBehaviour
 
         speed = velocity.magnitude;
 
+        Debug.Log(BankValue());
+
         if (speed > 0)
         {
-            transform.forward = velocity;
+
+            Vector3 tempUp = Vector3.Lerp(transform.up, Vector3.up + (acceleration * BankValue()), Time.deltaTime * 3.0f);
+            transform.LookAt(transform.position + velocity, tempUp);
         }
 
     }
